d48e26c7f4c9e4acbf34a10c1288ecaa8ee6e9ec
Mike Hummel <mh@mhus.de>
2016-11-04 21:41:45 +0100

    add tool

--- mhu-osgi-sop/mhu-osgi-sop-api/src/main/java/de/mhus/osgi/sop/api/aaa/AaaUtil.java
+++ mhu-osgi-sop/mhu-osgi-sop-api/src/main/java/de/mhus/osgi/sop/api/aaa/AaaUtil.java
@@ -39,4 +39,31 @@ public class AaaUtil {
 		
 	}
 
+	public static boolean hasAccess(Rightful accessControl, String[] acl) {
+		if (accessControl == null || acl == null )
+			return false;
+
+		try {
+			for (String line : acl) {
+				if (line.startsWith("not:")) {
+					line = line.substring(4);
+					if (accessControl.hasGroup(line)) return false;
+				} else
+				if (line.startsWith("notuser:")) {
+					line = line.substring(8);
+					if (accessControl.getName().equals(line)) return false;
+				} else
+				if (line.startsWith("user:")) {
+					line = line.substring(5);
+					if (accessControl.getName().equals(line)) return true;
+				} else
+				if (line.equals("*") || accessControl.hasGroup(line)) return true;
+			}
+		} catch (Throwable t) {
+			log.d(acl, accessControl, t);
+		}
+		return false;
+		
+	}
+
 }
