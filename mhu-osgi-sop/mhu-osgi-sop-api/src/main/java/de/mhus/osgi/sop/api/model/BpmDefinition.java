24f9ecde3b8664878b388c91df9452a46cbb515d
Mike Hummel <mikehummel@Mikes-MBP.box>
2016-11-22 07:20:19 +0100

    update abstract bpm connector

---
+++ mhu-osgi-sop/mhu-osgi-sop-api/src/main/java/de/mhus/osgi/sop/api/model/BpmDefinition.java
@@ -0,0 +1,72 @@
+package de.mhus.osgi.sop.api.model;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import de.mhus.lib.annotations.adb.DbIndex;
+import de.mhus.lib.annotations.adb.DbPersistent;
+import de.mhus.lib.core.MProperties;
+import de.mhus.lib.core.MString;
+import de.mhus.lib.errors.MException;
+import de.mhus.osgi.sop.api.model.DbMetadata;
+
+public class BpmDefinition extends DbMetadata {
+
+	@DbPersistent
+	@DbIndex("u1")
+	private String process;
+	@DbPersistent
+	@DbIndex("u1")
+	private String processor;
+	@DbPersistent
+	private HashSet<String> parameters = new HashSet<>();
+	@DbPersistent
+	private boolean enabled = true;
+	@DbPersistent
+	private MProperties options = new MProperties();
+	
+	public BpmDefinition() {
+	}
+	
+	public BpmDefinition(String processor) {
+		this.processor = processor;
+	}
+
+	@Override
+	public DbMetadata findParentObject() throws MException {
+		return null;
+	}
+
+	public String getProcess() {
+		return process;
+	}
+
+	public void setProcess(String process) {
+		this.process = process;
+	}
+
+	public HashSet<String> getParameters() {
+		return parameters;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public MProperties getOptions() {
+		return options;
+	}
+
+	public String getPublicFilter(boolean created) {
+		return created ? options.getString("filter.created", null) : options.getString("filter.public", null);
+	}
+
+	public String getProcessor() {
+		return processor;
+	}
+	
+}
--- mhu-osgi-sop/mhu-osgi-sop-impl/src/main/java/de/mhus/osgi/sop/impl/action/BpmActionProvider.java
+++ mhu-osgi-sop/mhu-osgi-sop-impl/src/main/java/de/mhus/osgi/sop/impl/action/BpmActionProvider.java
@@ -1,15 +1,35 @@
 package de.mhus.osgi.sop.impl.action;
 
 import java.util.Collection;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import de.mhus.lib.adb.DbManager;
+import de.mhus.lib.adb.query.Db;
 import de.mhus.lib.core.IProperties;
+import de.mhus.lib.core.MLog;
+import de.mhus.lib.core.MProperties;
+import de.mhus.lib.core.MTimeInterval;
+import de.mhus.lib.core.MValidator;
+import de.mhus.lib.core.cfg.CfgLong;
 import de.mhus.lib.core.strategy.Monitor;
 import de.mhus.lib.core.strategy.OperationResult;
+import de.mhus.lib.core.strategy.Successful;
+import de.mhus.lib.core.util.ParameterDefinition;
+import de.mhus.lib.errors.MException;
+import de.mhus.osgi.sop.api.Sop;
 import de.mhus.osgi.sop.api.action.Action;
 import de.mhus.osgi.sop.api.action.ActionDescriptor;
 import de.mhus.osgi.sop.api.action.ActionProvider;
+import de.mhus.osgi.sop.api.adb.AdbApi;
+import de.mhus.osgi.sop.api.model.BpmCase;
+import de.mhus.osgi.sop.api.model.BpmDefinition;
 
-public class BpmActionProvider implements ActionProvider {
+public abstract class BpmActionProvider extends MLog implements ActionProvider {
+
+	private CfgLong updateInterval = new CfgLong(BpmActionProvider.class, "updateInterval", MTimeInterval.MINUTE_IN_MILLISECOUNDS * 10 );
 
 	@Override
 	public <I> I adaptTo(Class<? extends Object> ifc) {
@@ -17,18 +37,40 @@ public class BpmActionProvider implements ActionProvider {
 		return null;
 	}
 
+	/**
+	 * Return the unique name of the provider
+	 */
 	@Override
-	public String getName() {
-		return "bpm";
-	}
+	public abstract String getName();
 
 	@Override
 	public Collection<ActionDescriptor> getActions() {
-		return null;
+
+		LinkedList<ActionDescriptor> out = new LinkedList<ActionDescriptor>();
+		try {
+			for (BpmDefinition def : Sop.getApi(AdbApi.class).getManager().getByQualification( Db.query(BpmDefinition.class).eq(BpmDefinition::getProcessor, getName()) )) {
+				LinkedList<String> tags = new LinkedList<>();
+				String tagsStr = def.getOptions().getString("tags",null);
+				if (tagsStr != null) {
+					for (String t : tagsStr.split(","))
+						tags.add(t);
+				}
+				out.add(new ActionDescriptor(new BpmAction(def), tags, getName(), def.getProcess() ));
+			}
+		} catch (Throwable t) {
+			log().e(t);
+		}
+		
+		return out;
 	}
 
-	
-	private static class BpmAction implements Action {
+	private class BpmAction implements Action {
+
+		private BpmDefinition def;
+
+		public BpmAction(BpmDefinition def) {
+			this.def = def;
+		}
 
 		@Override
 		public <I> I adaptTo(Class<? extends Object> ifc) {
@@ -38,21 +80,96 @@ public class BpmActionProvider implements ActionProvider {
 
 		@Override
 		public String getName() {
-			// TODO Auto-generated method stub
-			return null;
+			return def.getProcess();
 		}
 
 		@Override
 		public boolean canExecute(IProperties properties) {
-			// TODO Auto-generated method stub
-			return false;
+			return true;
 		}
 
 		@Override
 		public OperationResult doExecute(IProperties properties, Monitor monitor) throws Exception {
-			// TODO Auto-generated method stub
-			return null;
+
+			String process = def.getProcess();
+			String mapped = def.getOptions().getString("processMapping", process);
+			
+			MProperties parameters = new MProperties();
+			String customId = properties.getString("_customId", "");
+			
+			DbManager manager = Sop.getApi(AdbApi.class).getManager();
+			BpmCase caze = manager.inject(new BpmCase(null, BpmActionProvider.this.getName(), process, mapped, customId, parameters));
+			caze.save();
+
+			Map<String, ParameterDefinition> pDefs = ParameterDefinition.createDefinitions(def.getParameters());
+			
+			for (Entry<String, ParameterDefinition> pDefEntry : pDefs.entrySet()) {
+				String fromName = pDefEntry.getKey();
+				ParameterDefinition pDef = pDefEntry.getValue();
+				String toName = pDef.getMapping() == null ? fromName : pDef.getMapping();
+				
+				Object value = properties.get(fromName);
+				value = pDef.transform(value);
+				
+				parameters.put(toName, value);
+			}
+				
+			String id = createCase(mapped, parameters);
+			caze.setBpmId(id);
+			caze.save();
+			sync(id, true);
+			
+			return new Successful(caze.getProcess(), "ok", 0, caze);
 		}
 		
 	}
+
+	/**
+	 * Create a process with the given parameters and return the caseId
+	 * 
+	 * @param process Process name
+	 * @param parameters Parameter set
+	 * @return The id of the created process
+	 * @throws Exception 
+	 */
+	public abstract String createCase(String process, MProperties parameters) throws Exception;
+
+	public boolean sync(String id, boolean forced) {
+		try {
+			BpmCase caze = getCase(id);
+			if (caze == null) return false;
+		
+			Date last = caze.getLastSync();
+			if (!forced && last != null && System.currentTimeMillis() - last.getTime() < updateInterval.value()  )
+				return false;
+
+			if (syncCase(caze)) {
+				caze.setLastSync(new Date());
+				caze.save();
+				return true;
+			}
+			return false;
+			
+		} catch (Throwable t) {
+			log().d(t);
+			return false;
+		}
+	}
+
+	/**
+	 * Synchronize the case with the remote BPM. Do not save the case object
+	 * 
+	 * @param caze
+	 * @return true if sync was successful
+	 */
+	protected abstract boolean syncCase(BpmCase caze);
+
+	public BpmCase getCase(String id) throws MException {
+		DbManager manager = Sop.getApi(AdbApi.class).getManager();
+		if (MValidator.isUUID(id))
+			return manager.getObject(BpmCase.class, id);
+		else
+			return manager.getObjectByQualification((Db.query(BpmCase.class).eq(BpmCase::getProcessor, getName()).eq(BpmCase::getBpmId, id) ) );
+	}
+	
 }
