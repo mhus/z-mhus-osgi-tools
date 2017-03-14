e2b09c9b4de10656c14af3aba27737ec33d4f6d5
Mike Hummel <mikehummel@Nofretete.local>
2017-03-14 09:34:09 +0100

    add DLQ 2 Log Consumer

---
+++ mhu-osgi-jms/src/main/java/de/mhus/osgi/jms/DLQConsumer.java
@@ -0,0 +1,31 @@
+package de.mhus.osgi.jms;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+import de.mhus.lib.core.logging.Log;
+import de.mhus.lib.jms.JmsConnection;
+import de.mhus.lib.jms.JmsDestination;
+import de.mhus.lib.jms.ServerJms;
+
+public class DLQConsumer extends ServerJms {
+
+	Log log = Log.getLog(DLQConsumer.class);
+	
+	public DLQConsumer(JmsConnection con) {
+		super(new JmsDestination("ActiveMQ.DLQ", false).setConnection(con));
+	}
+
+	@Override
+	public void receivedOneWay(Message msg) throws JMSException {
+		log.w("Lost Message",msg);
+	}
+
+	@Override
+	public Message received(Message msg) throws JMSException {
+		receivedOneWay(msg);
+		return null;
+	}
+
+	
+}
