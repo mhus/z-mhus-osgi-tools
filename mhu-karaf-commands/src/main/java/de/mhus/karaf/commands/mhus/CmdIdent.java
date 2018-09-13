4dc2428ba130cf0d52a82b5a11d2a270c7de5f07
Mike Hummel <>
2018-09-13 17:08:26 +0200

    add tool

---
+++ mhu-karaf-commands/src/main/java/de/mhus/karaf/commands/mhus/CmdIdent.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2018 Mike Hummel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.mhus.karaf.commands.mhus;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+
+import de.mhus.lib.core.MApi;
+import de.mhus.lib.core.MSystem;
+import de.mhus.lib.core.service.ServerIdent;
+
+@Command(scope = "mhus", name = "ident", description = "Print the Server Ident")
+@Service
+public class CmdIdent implements Action {
+
+	@Option(name="-a", aliases="--all", description="Print also hostname and pid",required=false)
+	private boolean full;
+
+	@Override
+	public Object execute() throws Exception {
+		ServerIdent service = MApi.lookup(ServerIdent.class);
+		return service.toString() + (full ? " " + MSystem.getPid() + "@" + MSystem.getHostname() : "");
+	}
+
+}
