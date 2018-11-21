546cf0f2e597414fd9088e95846c190a1ff403d0
Mike Hummel <mike@mhus.de>
2018-11-21 22:27:41 +0100

    save

--- mhu-karaf-commands/src/main/java/de/mhus/karaf/commands/testit/JdbcMetaShit.java
+++ mhu-karaf-commands/src/main/java/de/mhus/karaf/commands/testit/JdbcMetaShit.java
@@ -1,6 +1,7 @@
 package de.mhus.karaf.commands.testit;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -35,7 +36,31 @@ public class JdbcMetaShit implements ShitIfc {
 			ResultSet res = con.getMetaData().getIndexInfo(str(parameters[1]), str(parameters[2]), str(parameters[3]), bool(parameters[4]), bool(parameters[5]));
 			out(res);
 			con.close();
-			
+		} else
+		if (cmd.equals("info")) {
+			Connection con = ds.getConnection();
+			DatabaseMetaData m = con.getMetaData();
+			System.out.println("IdentifierQuoteString: " + m.getIdentifierQuoteString());
+			System.out.println("CatalogSeparator: " + m.getCatalogSeparator());
+			System.out.println("CatalogTerm: " + m.getCatalogTerm());
+			System.out.println("DatabaseMajorVersion: "+m.getDatabaseMajorVersion());
+			System.out.println("DatabaseMinorVersion: "+m.getDatabaseMinorVersion());
+			System.out.println("DatabaseProductName: "+m.getDatabaseProductName());
+			System.out.println("DefaultTransactionIsolation: "+m.getDefaultTransactionIsolation());
+			System.out.println("DriverMajorVersion: "+m.getDriverMajorVersion());
+			System.out.println("DriverMinorVersion: "+m.getDriverMinorVersion());
+			System.out.println("DriverName: "+m.getDriverName());
+			System.out.println("URL: "+m.getURL());
+			System.out.println("DatabaseProductVersion: "+m.getDatabaseProductVersion());
+			System.out.println("UserName: "+m.getUserName());
+			System.out.println("ExtraNameCharacters: "+m.getExtraNameCharacters());
+			System.out.println("JDBCMajorVersion: "+m.getJDBCMajorVersion());
+			System.out.println("JDBCMinorVersion: "+m.getJDBCMinorVersion());
+			System.out.println("MaxBinaryLiteralLength: "+m.getMaxBinaryLiteralLength());
+			System.out.println("MaxCatalogNameLength: "+m.getMaxCatalogNameLength());
+			System.out.println("MaxBinaryLiteralLength: "+m.getMaxBinaryLiteralLength());
+			System.out.println("MaxColumnNameLength: "+m.getMaxColumnNameLength());
+			con.close();
 		}
 		return null;
 	}
@@ -61,12 +86,12 @@ public class JdbcMetaShit implements ShitIfc {
 	}
 
 	private String[] strarr(String string) {
-		if (string.equals("null")) return null;
+		if (string == null || string.equals("null")) return null;
 		return string.split(",");
 	}
 
 	private String str(String string) {
-		if (string.equals("null")) return null;
+		if (string ==  null || string.equals("null")) return null;
 		return string;
 	}
 
