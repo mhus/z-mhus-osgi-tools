289504049d63c54993a50d18c040d1861f2e0d07
Mike Hummel <mike@mhus.de>
2020-06-03 09:07:29 +0200

    Update AccessApiService.java

--- osgi-services/src/main/java/de/mhus/osgi/services/shiro/AccessApiService.java
+++ osgi-services/src/main/java/de/mhus/osgi/services/shiro/AccessApiService.java
@@ -1,11 +1,15 @@
 package de.mhus.osgi.services.shiro;
 
+import java.util.HashMap;
+
 import org.apache.commons.beanutils.ConvertUtilsBean;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.env.DefaultEnvironment;
 import org.osgi.service.component.annotations.Component;
 
 import de.mhus.lib.core.shiro.AccessApi;
 import de.mhus.lib.core.shiro.DefaultAccessApi;
+import de.mhus.lib.core.shiro.EmptySecurityManager;
 
 @Component(service = AccessApi.class)
 public class AccessApiService extends DefaultAccessApi {
@@ -14,7 +18,14 @@ public class AccessApiService extends DefaultAccessApi {
     protected void initialize() {
         new ConvertUtilsBean();
         log().d("Initialize Shiro",CFG_CONFIG_FILE);
-        env = new MyBasicIniEnvironment(CFG_CONFIG_FILE.value());
+        try {
+        	env = new MyBasicIniEnvironment(CFG_CONFIG_FILE.value());
+        } catch (Exception e) {
+    		log().d("Initialize empty shiro",CFG_CONFIG_FILE, e.toString());
+    		HashMap<String, Object> seed = new HashMap<>();
+    		seed.put(DefaultEnvironment.DEFAULT_SECURITY_MANAGER_KEY, new EmptySecurityManager());
+    		env = new DefaultEnvironment(seed);
+        }
         SecurityUtils.setSecurityManager(env.getSecurityManager());
 //        Factory<SecurityManager> factory = new IniSecurityManagerFactory(CFG_CONFIG_FILE.value());
 //        securityManager = factory.getInstance();
