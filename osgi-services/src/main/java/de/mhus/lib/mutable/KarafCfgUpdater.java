007250c1e1a274ca1edd2a660693db0aea4b4046
Mike Hummel <mike@mhus.de>
2020-06-08 22:00:13 +0200

    Update KarafCfgUpdater.java

--- osgi-services/src/main/java/de/mhus/lib/mutable/KarafCfgUpdater.java
+++ osgi-services/src/main/java/de/mhus/lib/mutable/KarafCfgUpdater.java
@@ -1,6 +1,8 @@
 package de.mhus.lib.mutable;
 
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventHandler;
@@ -11,12 +13,20 @@ import de.mhus.osgi.api.services.MOsgi;
 @Component(property = "event.topics=org/osgi/framework/ServiceEvent/MODIFIED")
 public class KarafCfgUpdater implements EventHandler {
 
+	
+	private BundleContext ctx;
+
+	@Activate
+	public void doActivate(BundleContext ctx) {
+		this.ctx = ctx;
+	}
+
     @Override
     public void handleEvent(Event event) {
         try {
             @SuppressWarnings("unchecked")
             ServiceReference<Object> serviceRef = (ServiceReference<Object>) event.getProperty("service");
-            Object service = MOsgi.getService(serviceRef);
+            Object service = ctx.getService(serviceRef);
             String pid = MOsgi.getPid(service.getClass());
             ((KarafCfgManager)MApi.get().getCfgManager()).update(pid);
             MApi.getCfgUpdater().doUpdate(pid);
