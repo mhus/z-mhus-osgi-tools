460aca8fa1a1ac62b53840c6c91cd9a10b162943
Mike Hummel <mike.hummel@billingsolutions.de>
2021-08-25 16:20:54 +0200

    Update OsgiCacheManager.java

--- osgi-services/src/main/java/de/mhus/osgi/services/cache/OsgiCacheManager.java
+++ osgi-services/src/main/java/de/mhus/osgi/services/cache/OsgiCacheManager.java
@@ -27,23 +27,20 @@ import org.apache.shiro.cache.Cache;
 import org.apache.shiro.cache.CacheException;
 import org.apache.shiro.cache.CacheManager;
 
-import de.mhus.lib.core.M;
 import de.mhus.lib.core.MCollection;
 import de.mhus.lib.core.MPeriod;
 import de.mhus.lib.core.aaa.AccessApi;
-import de.mhus.lib.core.cache.CacheConfig;
 import de.mhus.lib.core.cache.ICache;
-import de.mhus.lib.core.cache.ICacheService;
 import de.mhus.lib.core.cfg.CfgBoolean;
-import de.mhus.lib.core.cfg.CfgInt;
 import de.mhus.lib.core.cfg.CfgLong;
+import de.mhus.lib.core.util.SoftTimeoutMap;
 
 public class OsgiCacheManager implements CacheManager {
 
     private static CfgLong CFG_TTL =
             new CfgLong(
                     AccessApi.class, "authorizationCacheTTL", MPeriod.MINUTE_IN_MILLISECOUNDS * 30);
-    private static CfgInt CFG_SIZE = new CfgInt(AccessApi.class, "authorizationCacheSize", 100000);
+//    private static CfgInt CFG_SIZE = new CfgInt(AccessApi.class, "authorizationCacheSize", 100000);
     private static CfgBoolean CFG_ENABLED =
             new CfgBoolean(AccessApi.class, "authorizationCacheEnabled", true);
 
@@ -58,21 +55,65 @@ public class OsgiCacheManager implements CacheManager {
         Cache<K, V> inst = caches.get(name);
         if (inst != null) return (Cache<K, V>) inst;
 
-        ICacheService service = M.l(ICacheService.class);
-        if (service == null) return null;
-        ICache<Object, Object> c =
-                service.createCache(
-                        this,
-                        name,
-                        Object.class,
-                        Object.class,
-                        new CacheConfig().setHeapSize(CFG_SIZE.value()).setTTL(CFG_TTL.value()));
+//        ICacheService service = M.l(ICacheService.class);
+//        if (service == null) return null;
+//        ICache<Object, Object> c =
+//                service.createCache(
+//                        this,
+//                        name,
+//                        Object.class,
+//                        Object.class,
+//                        new CacheConfig().setHeapSize(CFG_SIZE.value()).setTTL(CFG_TTL.value()));
 
-        inst = new CacheWrapper<>(c);
+//        inst = new CacheWrapper<>(c);
+      inst = new LocalCache<>();
         caches.put(name, inst);
         return inst;
     }
 
+    private static class LocalCache<K, V> implements Cache<K, V> {
+
+        SoftTimeoutMap<K, V> cache = new SoftTimeoutMap<>(CFG_TTL.value());
+
+        @Override
+        public void clear() throws CacheException {
+            cache.clear();
+        }
+
+        @Override
+        public V get(K arg0) throws CacheException {
+            return cache.get(arg0);
+        }
+
+        @Override
+        public Set<K> keys() {
+            return cache.keySet();
+        }
+
+        @Override
+        public V put(K arg0, V arg1) throws CacheException {
+            return cache.put(arg0, arg1);
+        }
+
+        @Override
+        public V remove(K arg0) throws CacheException {
+            return cache.remove(arg0);
+        }
+
+        @Override
+        public int size() {
+            return cache.size();
+        }
+
+        @Override
+        public Collection<V> values() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+        
+    }
+    
+    @SuppressWarnings("unused")
     private static class CacheWrapper<K, V> implements Cache<K, V> {
 
         private ICache<Object, Object> inst;
